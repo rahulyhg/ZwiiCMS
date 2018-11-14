428c0f73240d7da8c809b9499e0a1b0f99276309
fredtempez <ftempez@gmail.com>
2018-11-14 15:40:46 +0100

    8.4.3 : correction site map et url metaimage

--- core/core.php
+++ core/core.php
@@ -24,7 +24,7 @@ class common {
 	const GROUP_MEMBER = 1;
 	const GROUP_MODERATOR = 2;
 	const GROUP_ADMIN = 3;
-	const ZWII_VERSION = '8.4.2';
+	const ZWII_VERSION = '8.4.3';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
@@ -2015,7 +2015,7 @@ class layout extends common {
 	 * Affiche la meta image (site screenshot)
 	 */
 	public function showMetaImage() {		
-		echo '<meta property="og:image" content="' . helper::baseUrl() .'site/screenshot.png" />';
+		echo '<meta property="og:image" content="' . helper::baseUrl() .'/site/file/source/screenshot.png" />';
 	}
 
 
--- core/module/sitemap/view/index/index.php
+++ core/module/sitemap/view/index/index.php
@@ -1,14 +1,14 @@
 <ul>
 	<?php foreach($this->getHierarchy() as $parentId => $childIds): ?>
 		<li>
-			<?php if ($this->getData(['page', $parentId, 'disable']) === false) { ?>
+			<?php if ($this->getData(['page', $parentId, 'disable']) == false) { ?>
 				<a href="<?php echo helper::baseUrl() . $parentId; ?>"><?php echo $this->getData(['page', $parentId, 'title']); ?></a>
 			<?php } else { ?>
 				<?php echo $this->getData(['page', $parentId, 'title']); }?>
 			<ul>
 				<?php foreach($childIds as $childId): ?>
 					<li>
-						<?php if ($this->getData(['page', $childId, 'disable']) === false) { ?>
+						<?php if ($this->getData(['page', $childId, 'disable']) == false) { ?>
 							<a href="<?php echo helper::baseUrl() . $childId; ?>"><?php echo $this->getData(['page', $childId, 'title']); ?></a>
 						<?php } else { ?>
 							<?php echo $this->getData(['page', $childId, 'title']); }?>						
