f2cfb4068a10e13b51e29b50d7e1a994a498538e
fredtempez <ftempez@gmail.com>
2019-01-10 18:02:27 +0100

    Modfi numéro de version

--- core/core.php
+++ core/core.php
@@ -24,7 +24,7 @@ class common {
 	const GROUP_MEMBER = 1;
 	const GROUP_MODERATOR = 2;
 	const GROUP_ADMIN = 3;
-	const ZWII_VERSION = '8.5.4';
+	const ZWII_VERSION = '8.5.5';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
@@ -360,7 +360,7 @@ class common {
 				'backgroundColor' => 'rgba(255, 255, 255, 1)',
 				'radius' => '0',
 				'shadow' => '0',
-				'width' => '1170px'
+				'width' => '750px'
 			],
 			'text' => [
 				'font' => 'Open+Sans',
