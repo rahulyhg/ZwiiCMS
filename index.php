eea361bd6ac35f5b15052309855992d2372450f7
fredtempez <ftempez@gmail.com>
2019-01-18 10:35:39 +0100

    setlocal fr

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(version_compare(PHP_VERSION, '5.6.0', '<')) {
 
 /* Set locale to French */
 date_default_timezone_set('Europe/Paris');
-setlocale (LC_TIME, 'fra_fra', 'french');
+setlocale (LC_TIME, 'fr_FR', 'fra');
 
 /**
  * Initialisation de Zwii
