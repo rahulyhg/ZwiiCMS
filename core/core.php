20cb5152d9cd21eccb200b0eca13002eb36c3baf
fredtempez <ftempez@gmail.com>
2019-02-19 23:51:42 +0100

    mise à jour de version dev12

--- core/core.php
+++ core/core.php
@@ -28,7 +28,7 @@ class common {
 	const GROUP_ADMIN = 3;
 	// Numéro de version de développement :/
 	// Désactive l'update auto
-	const ZWII_VERSION = '9.0.00-dev11';
+	const ZWII_VERSION = '9.0.00-dev12';
 	// Numéro de version stable
 	//const ZWII_VERSION = '9.0.00';
 
