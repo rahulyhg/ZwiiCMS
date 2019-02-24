52172b689196057a60e9896154b4f812932fb99a
fredtempez <ftempez@gmail.com>
2019-02-24 19:10:32 +0100

    Taille miniature 266x200

--- core/vendor/filemanager/UploadHandler.php
+++ core/vendor/filemanager/UploadHandler.php
@@ -1437,7 +1437,7 @@ class UploadHandler
 				$magicianObj -> saveImage($targetFile);
 			}
 
-			$thumbResult = create_img($targetFile, $targetFileThumb, 200, 150);
+			$thumbResult = create_img($targetFile, $targetFileThumb, 266, 200);
 
 			if ( $thumbResult!==true)
 			{
