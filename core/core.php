ed049afc101bbeb7908e7c03776a647923f55db6
fredtempez <ftempez@gmail.com>
2019-02-28 15:03:36 +0100

    dev24 : modification body  margin

--- core/core.php
+++ core/core.php
@@ -28,7 +28,7 @@ class common {
 	const GROUP_ADMIN = 3;
 	// Numéro de version de développement :/
 	// Désactive l'update auto
-	const ZWII_VERSION = '9.0.00-dev23';
+	const ZWII_VERSION = '9.0.00-dev24';
 	// Numéro de version stable
 	//const ZWII_VERSION = '9.0.00';
 
