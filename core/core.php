dc673982cec840500674bfdf28db5f017ec9cb9d
fredtempez <ftempez@gmail.com>
2019-03-27 19:11:58 +0100

    [9.1.00-dev7] lighter css effects

--- core/core.php
+++ core/core.php
@@ -28,7 +28,7 @@ class common {
 	const GROUP_ADMIN = 3;
 
 	// Numéro de version stable
-	const ZWII_VERSION = '9.1.00-dev6';
+	const ZWII_VERSION = '9.1.00-dev7';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
