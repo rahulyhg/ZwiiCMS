7e59fb42c8c2e2b64bc4af6cdcf99329a3e05ab8
fredtempez <ftempez@gmail.com>
2019-04-02 18:36:18 +0200

    [9.0.16] version

--- core/core.php
+++ core/core.php
@@ -28,7 +28,7 @@ class common {
 	const GROUP_ADMIN = 3;
 
 	// Numéro de version stable
-	const ZWII_VERSION = '9.0.15';
+	const ZWII_VERSION = '9.0.16';
 
 	public static $actions = [];
 	public static $coreModuleIds = [
